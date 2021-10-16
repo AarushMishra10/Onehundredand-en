58b97775f2a283bbd6f14239603e4ab6f256288e
AarushMishra10 <77782297+AarushMishra10@users.noreply.github.com>
2021-10-16 11:23:42 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,38 @@
+prediction1="";
+prediction2="";
+
+Webcam.set({
+    width: 350,
+    height: 300,
+    image_format: 'png',
+    png_quality: 90 
+});
+camera=document.getElementById("camera");
+Webcam.attach("#camera");
+
+function snapshot(){
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML='<img id="captured_image" src="'+data_uri+'">';
+
+    });
+}
+console.log('ml5 version:', ml5.version);
+classifier=ml5.imageClassifier("",modelLoaded);
+
+function modelLoaded(){
+    console.log('modelLoaded');
+
+}
+
+function speak(){
+    synth=window.speechSynthesis;
+    Speakdata1= "The first prediction is "+prediction1;
+    Speakdata2= "The second prediction is"+prediction2;
+    utterthis=new SpeechSynthesisUtterance(speakdata1+speakdata2);
+}
+
+function check(){
+    img=document.getElementById("captured_image");
+    classifier.classify(img,gotresults);
+
+}
